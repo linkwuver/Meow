5302917b525f16bf072f11029042c241ecbc260a
linkwuver <teanguyen423@gmail.com>
2017-05-07 15:30:40 -0700

    added unity file

---
+++ RPG Unity/Assets/Scripts/DisableFPS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+//attach this script to gameobject to be picked up.
+//call disable controller from wherever you want to de-activate the FPS controller.
+//make sure box collider istrigger is set to true.
+
+public class DisableFPS : MonoBehaviour //drag and drop FPS controller gameobject in the public fps controller field in the inspector.
+{
+    public FirstPersonController FPSController;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //make sure that you set your FPSController object tag (in your hierarchy) to "Player"
+        
+
+        if (other.tag == "Player")
+        {
+            DisableController();
+        }
+    }
+
+    public void DisableController()
+    {
+        FPSController.enabled = true;
+    }
+}
---
+++ RPG Unity/Assets/Scripts/GrabObject.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+
+    GameObject grabbedObject;
+    float grabbedObjectSize;
+    Rigidbody grabbedObjectRigidBody;
+
+    GameObject GetMouseHoverObject(float range)
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit raycastHit;
+        Vector3 target = position + Camera.main.transform.forward * range;
+
+        if (Physics.Linecast(position, target, out raycastHit))
+            return raycastHit.collider.gameObject;
+        return null;
+
+    }
+
+    void TryGrabObject(GameObject grabObject)
+    {
+        if (grabObject == null || !CanGrab(grabObject))
+            return;
+        grabbedObject = grabObject;
+        Debug.Log(grabbedObject.ToString());
+        grabbedObjectSize = grabObject.GetComponent<Renderer>().bounds.size.magnitude;
+        grabbedObjectRigidBody = grabbedObject.GetComponent<Rigidbody>();
+    }
+    bool CanGrab(GameObject canidate)
+    {
+        return canidate.GetComponent<Rigidbody>();
+    }
+    void DropObject()
+    {
+        if (grabbedObject == null)
+            return;
+
+
+        if (grabbedObject.GetComponent<Rigidbody>() != null)
+            grabbedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        grabbedObject = null;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (grabbedObject == null)
+                TryGrabObject(GetMouseHoverObject(5));
+
+            else
+                DropObject();
+            if (grabbedObjectRigidBody != null)
+            {
+                if (grabbedObjectRigidBody.freezeRotation == true)
+                {
+                    grabbedObjectRigidBody.freezeRotation = false;
+                }
+            }
+        }
+
+        if (grabbedObject != null)
+        {
+            Vector3 newPosition = gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize;
+            grabbedObjectRigidBody.MovePosition(newPosition);
+            grabbedObjectRigidBody.freezeRotation = true;
+        }
+
+    }
+}
